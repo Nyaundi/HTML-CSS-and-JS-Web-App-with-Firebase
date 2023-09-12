4c7bda7a575faf9019dfaec22e2dee7db0a33181
Nyaundi <jamesmabuka260@gmail.com>
2023-09-12 11:15:05 +0300

    x

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-import { add } from "../functions.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettig = {
+const appSettings = {
     databaseURL: "https://freecodecamp-firebase-app-default-rtdb.firebaseio.com/"
 }
 
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInBD = ref(database, "shoppingList")
 
 const addButtonEl = document.getElementById("input-field");
 const addinput = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function(){
     let cleanCode = addButtonEl.value
-    console.log(cleanCode);
+
+    push(shoppingListInBD, cleanCode)
+
+    console.log(`${cleanCode} is added`);
 })
 
-console.log(add(2, 3));
